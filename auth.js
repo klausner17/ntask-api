9e74ca5fef31cc524430d35fa25db6025f087ae0
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-01 13:51:46 -0200

    até a pagina 89

--- auth.js
+++ auth.js
@@ -1,12 +1,12 @@
 import passport from "passport";
-import { Strategy, ExtractJwt } from "passport-jwt";
+import {Strategy, ExtractJwt} from "passport-jwt";
 
 module.exports = app => {
     const Users = app.db.models.Users;
     const cfg = app.libs.config;
     const params = {
         secretOrKey: cfg.jwtSecret,
-        jwtFromRequest: ExtractJwt.fromAuthHeader()
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
     };
     const strategy = new Strategy(params, (payload, done) => {
         Users.findById(payload.id)
