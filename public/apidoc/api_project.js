90f4b4e17655069cbeb3ea0bf08f4577a4853ba8
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-03 20:25:02 -0200

    apidoc

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "Documentação - Node Task API",
+  "template": {
+    "forceLanguage": "pt_br"
+  },
+  "version": "1.0.0",
+  "description": "API  de gestão de tarefas",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-11-03T22:23:35.200Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
