040723afc22a78cb05bab6cc025a2b76f3dd2a36
Klausner Augusto <klausnercampos@tokenlab.com.br>
2017-11-06 13:28:34 -0200

    Finalizando documentação

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,32 @@ module.exports = app => {
 
   app.route("/tasks")
     .all(app.auth.authenticate())
+    /**
+     * @api {get} /tasks Lista de tarefas
+     * @apiGroup Tarefas
+     * @apiHeader {String} Authorization Token de Usuário
+     * @apiHeaderExample {json} Header
+     *  {"Authorization", "Bearer xyz.abc.123.hgf"}
+     * @apiSuccess (200) {Object[]} tasks Lista de tarefas
+     * @apiSuccess (200) {Number} tasks.id Id do registro
+     * @apiSuccess (200) {String} tasks.title Titulo da tarefa
+     * @apiSuccess (200) {Boolean} tasks.done Tarefa foi concluída?
+     * @apiSuccess (200) {Date} tasks.created_at Data da criação da tarefa
+     * @apiSuccess (200) {Date} tasks.updated_at Data de atualização da tarefa
+     * @apiSuccess (200) {Number} tasks.user_id Id do usuário
+     * @apiSuccessExample {json} Success-Response:
+     *   HTTP/1.1 200 OK
+     *   [{
+     *       "id": 1,
+     *       "title": "Estudar",
+     *       "done": false;
+     *       "updated_at": "2015-05-11T12:30:00.778Z",
+     *       "created_at": "2015-05-11T12:30:00.778Z",
+     *       "user_id": 1
+     *   }]
+     * @apiErrorExample {json} Error-Response:
+         HTTP/1.1 412 Precondition Failed
+     */
     .get((req, res) => {
       Tasks.findAll({where: {user_id: req.user.id}})
         .then(result => res.json(result))
@@ -10,6 +36,34 @@ module.exports = app => {
           res.status(402).json({msg: error.message});
         });
     })
+    /**
+     * @api {post} /tasks Cadastra uma tarefa
+     * @apiGroup Tarefas
+     * @apiHeader (200) {String} Authorization Token de usuário
+     * @apiHeaderExample {json} Request-Example:
+     *   {"Authorization": "Bearer xyz.abc.123.ghf"}
+     * @apiParam  {String} title Título da tareda
+     * @apiParamExample  {json} Request-Example:
+     *   {"title": "Estudar"}    
+     * @apiSuccess (200) {Number} id Id da tarefa
+     * @apiSuccess (200) {String} title Titulo da tarefa
+     * @apiSuccess (200) {Boolean} done Tarefa concluída?
+     * @apiSuccess (200) {Date} updated_at Data de atualização
+     * @apiSuccess (200) {Date} created_at Data de criação
+     * @apiSuccess (200) {Number} user_id Id do Usuário
+     * @apiSuccessExample {json} Success-Response:
+     *   HTTP/1.1 200 OK
+     *   {
+     *       "id": 1,
+     *       "title": "Estudar",
+     *       "done": false,
+     *       "updated_at": "2017-05-11T12:30:00.778Z",
+     *       "created_at": "2017-05-11T12:30:00.778Z",
+     *       "user_id": 1
+     *   }
+     * @apiErrorExample {json} Error-Response:
+     *   HTTP/1.1 412 Precondition Failed
+     */
     .post((req, res) => {
       req.body.user_id = req.user.id;
       Tasks.create(req.body)
@@ -21,6 +75,34 @@ module.exports = app => {
 
   app.route("/tasks/:id")
     .all(app.auth.authenticate())
+    /**
+     * @api {get} /tasks/:id Exibe uma tarefa
+     * @apiGroup Tarefas
+     * @apiHeader (200) {String} Authorization Token do usuário
+     * @apiHeaderExample {json} Request-Example:
+     *   {"Authorization": "Bearer abc.xwz.123.klj"}
+     * @apiParam  {id} id Id do registro
+     * @apiSuccess (200) {Number} id Id da tarefa
+     * @apiSuccess (200) {String} title Titulo da tarefa
+     * @apiSuccess (200) {Boolean} done Tarefa concluída?
+     * @apiSuccess (200) {Date} updated_at Data de atualização
+     * @apiSuccess (200) {Date} created_at Data de criação
+     * @apiSuccess (200) {Number} user_id Id do Usuário
+     * @apiSuccessExample {json} Success-Response:
+     *   HTTP/1.1 200 OK
+     *   {
+     *       "id": 1,
+     *       "title": "Estudar",
+     *       "done": false,
+     *       "updated_at": "2017-05-11T12:30:00.778Z",
+     *       "created_at": "2017-05-11T12:30:00.778Z",
+     *       "user_id": 1
+     *   }
+     * @apiErrorExample {json} Not Found:
+     *   HTTP/1.1 404 Not Found
+     * @apiErrorExample {json} Error-Response:
+     *   HTTP/1.1 412 Precondition Failed
+     */
     .get((req, res) => {
       Tasks.findOne({where: {
           id: req.params.id,
@@ -37,6 +119,25 @@ module.exports = app => {
           res.status(412).json({msg: error.message});
         });
     })
+    /**
+     * @api {put} /tasks/:id Atualiza uma tarefa
+     * @apiGroup Tarefas
+     * @apiHeader (200) {String} Authorization Token do usuário
+     * @apiHeaderExample {json} Request-Example:
+     *   {"Authorization": "Bearer abc.xwz.123.klj"}
+     * @apiParam  {Number} id Id da tarefa
+     * @apiParam  {String} title Titulo da tarefa
+     * @apiParam  {Boolean} done Tarefa concluida?
+     * @apiParamExample  {json} Request-Example:
+     *   {
+     *       "title": "Trabalhar",
+     *       "done": true
+     *   }
+     * @apiSuccessExample {json} Success-Response:
+     *    HTTP/1.1 204 No Content
+     * @apiErrorExample {json} Error-Response:
+         HTTP/1.1 412 Precondition Failed
+     */
     .put((req, res) => {
       Tasks.update(req.body, { where: {
           id: req.params.id,
@@ -47,6 +148,18 @@ module.exports = app => {
           res.status(412).json({msg: error.message});
         });
     })
+    /**
+     * @api {delete} /tasks/:id Exclui uma tarefa
+     * @apiGroup Tarefas
+     * @apiHeader (200) {String} Authorization Token do usuário
+     * @apiHeaderExample {json} Request-Example:
+     *   {"Authorization": "Bearer abc.xwz.123.klj"}
+     * @apiParam  {Number} id Id da tarefa
+     * @apiSuccessExample {json} Success-Response:
+     *    HTTP/1.1 204 No Content
+     * @apiErrorExample {json} Error-Response:
+         HTTP/1.1 412 Precondition Failed
+     */
     .delete((req, res) => {
       Tasks.destroy({ where: {
           id: req.params.id,
